c51f9e8f906c2042c0d390f7fb586e07c6e2bbcb
Alhaithamxd <nomann177013@gmail.com>
2024-10-04 11:54:17 +0530

    fibo

--- array1/FibonacciUsingarray.java
+++ array1/FibonacciUsingarray.java
@@ -4,12 +4,14 @@ public class FibonacciUsingarray{
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        int [] numbers=new int[n];
+        int [] numbers=new int[n+1];
         numbers[0]=0;
         numbers[1]=1;
+        System.out.print(numbers[0]+", "+numbers[1]);
+        
         for(int i=2;i<numbers.length;i++){
             numbers[i]=numbers[i-1]+numbers[i-2];
-            System.out.println(numbers[i]);
+            System.out.print(", "+numbers[i]);
         }
     }
 }
