9f53c9a8646dde2ba6c6d46088892d7842992bf7
Alhaithamxd <nomann177013@gmail.com>
2024-10-14 18:08:01 +0530

    optimized

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -4,13 +4,19 @@ public class bubblesort{
         int n=numbers.length;
         int swap=0;
         for(int i=0;i<=n-2;i++){
+            boolean swap1=false;
             for(int j=0;j<=n-2-i;j++){
+                
                 if(numbers[j]>numbers[j+1]){
+                    swap1=true;
                     swap+=1;
                     int temp=numbers[j];
                     numbers[j]=numbers[j+1];
                     numbers[j+1]=temp;
                 }
+            }
+            if(swap1=false){
+                break;
             }
         }
         System.out.println(swap);
